5da68214da1d48b561a8e834f050c5c035e06dc3
unknown <hasib8nh@gmail.com>
2023-03-20 22:09:23 +0600

    Creating useEffect state to show bodypartsLists from the API

--- src/components/SearchExercises.js
+++ src/components/SearchExercises.js
@@ -6,8 +6,20 @@ import { exerciseOptions, fetchData } from '../utils/fetchData';
 const SearchExercises = () => {
 const [search, setSearch] = useState('');
 const [exercises, setExercises] = useState([]);
+const [bodyParts, setBodyParts] = useState([]);
+
+  useEffect(() => {
+    const fetchExercisesData = async () => {
+      const bodyPartsData = await fetchData('https://exercisedb.p.rapidapi.com/exercises/bodyPartList', exerciseOptions);
+
+      setBodyParts(['all', ...bodyPartsData]);
+    };
+
+    fetchExercisesData();
+  }, []);
 
 const handleSearch = async() => {
+
     if(search) {
         const exercisesData = await fetchData('https://exercisedb.p.rapidapi.com/exercises', exerciseOptions);
         console.log(exercisesData);
